72da61ac8403ef2e191ca2d091c2956fa9e94e14
JarvisAppleSlice <Samuel.s.jarvis@gmail.com>
2026-01-07 12:31:04 -0700

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+const qaItems = [
+	{
+		question: "How do I track my package?",
+		answer:
+			"You can easily track your package using our online tracking number on our website to get real-time updates on your deliverie's status",
+	},
+	{
+		question: "What should I do if my package is damaged or lost?",
+		answer:
+			"If your package arrives damaged or lost in transit, please contact us immediately. We will investigate the matter and arrange for a replacement or refund as per our policy.",
+	},
+	{
+		question: "Can I change my delivery address after placing an order?",
+		answer:
+			"Yes, you can change your delivery address as long as the package has not been dispatched. Please contact our customer service team as soon as possible to make any changes.",
+	},
+	{
+		question: "Are there any items that cannot be shipped?",
+		answer:
+			"Yes. there are certain restrictions on items that can be shipped due to safety and legal reasons. Please refer to our shipping policy or contact us for more information on prohibited items",
+	},
+];
+
+const accordionDiv = document.getElementById("accordion");
